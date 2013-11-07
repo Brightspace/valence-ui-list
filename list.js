de461f747ec0965b05c74ebf5e352fb1db5be15e
Dave Batiste <dave.batiste@Desire2Learn.com>
2013-11-07 17:44:57 -0500

    Defining List widget for VUI that can do simple state wire-up for selection.

---
+++ list.js
@@ -0,0 +1,42 @@
+﻿( function( $ ) {
+
+	$.widget( 'vui.vui_list', {
+
+		_create: function() {
+
+			var $node = $( this.element );
+
+			$node.find( 'li.vui-checkbox > label > input' ).each(
+				function ( index, inputNode ) {
+					
+					var $input = $( inputNode );
+					
+					$input.closest( 'li.vui-checkbox' ).toggleClass(
+						'vui-selected', $input.prop( 'checked' )
+					);
+					
+				}
+			);
+
+			$node.change( function( args ) {
+
+				var $target = $( args.target );
+
+				$target.closest( 'li.vui-checkbox' ).toggleClass(
+					'vui-selected', $target.prop( 'checked' ) 
+				);
+
+			} );
+
+		}
+
+	} );
+
+	vui.addClassInitializer(
+			'vui-list',
+			function( node ) {
+				$( node ).vui_list();
+			}
+		);
+
+} )( window.jQuery );
