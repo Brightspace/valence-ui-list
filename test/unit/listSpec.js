f46d23ef024cf07666ff78cb82454d6ceacb7bf3
Dave Batiste <dave.batiste@d2l.com>
2015-06-05 16:36:04 -0400

    Adding more selector tests.

--- test/unit/listSpec.js
+++ test/unit/listSpec.js
@@ -13,12 +13,28 @@
 				expect(document).toHaveCssSelector('.vui-list');
 			});
 
+			it('defines a ".vui-list > li" selector', function() {
+				expect(document).toHaveCssSelector('.vui-list > li');
+			});
+
 			it('defines a ".vui-list.vui-compact > li" selector', function() {
 				expect(document).toHaveCssSelector('.vui-list.vui-compact > li');
 			});
 
 			it('defines a ".vui-list.vui-no-separator > li" selector', function() {
 				expect(document).toHaveCssSelector('.vui-list.vui-no-separator > li');
+			});
+
+			it('defines a ".vui-list > .vui-selected" selector', function() {
+				expect(document).toHaveCssSelector('.vui-list > .vui-selected');
+			});
+
+			it('defines a ".vui-list > .vui-active" selector', function() {
+				expect(document).toHaveCssSelector('.vui-list > .vui-active');
+			});
+
+			it('defines a ".vui-list > .vui-active.vui-selected" selector', function() {
+				expect(document).toHaveCssSelector('.vui-list > .vui-active.vui-selected');
 			});
 
 		});
